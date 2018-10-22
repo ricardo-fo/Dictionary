bf69763edf4b3c42fc3a77343e921c1c6d965b5a
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-10-22 14:17:28 -0300

    web scraping - I'm gonna fix it

--- oxford-scraping.py
+++ oxford-scraping.py
@@ -3,59 +3,68 @@ This is a simples web scrape. It's search for words you type. The code get the H
 Oxford Dictionaries -> https://en.oxforddictionaries.com
 Author: Ricardo Oliveira
 """
-
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 import requests
 
-def linha():
+#=====================================================================
+def mainLoop():
 	print('-' * 50)
+	print('-' * 50)
+	print("\nEncontre o significado de palavras inglesas.\n")
+	print("Fazendo scraping de https://en.oxforddictionaries.com :)\nDigite 'sair' para sair.\n")
+	exp = 'a'
+	while True:
+		print('-' * 50)
+		print('-' * 50)
+		exp = input("\nDigite uma palavra em inglês: ")
+		if exp == 'sair':
+			print("\nBye :)")
+			break
+		URL = ErrorTest(exp) 
+		if URL != None:
+			print('\n')
+			translator(URL)
 
-def translator(word):
-	if(' ' in word):
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def ErrorTest(word):
+	if ' ' in word:
 		word = word.replace(' ', '-')
 
-	#Download HTML files
 	URL = "https://en.oxforddictionaries.com/definition/" + word
-	page = requests.get(URL, timeout = 5)
+	try:
+		html = urlopen(URL)
+	except HTTPError as err:
+		print(err)
+		return None
+	except URLError:
+		print("Server down or incorrect domain!\n")
+		return None
+	except Exception:
+		return None
+	else:
+		return URL
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def translator(site):
+	page = requests.get(site, timeout = 5)
 	soup = BeautifulSoup(page.content, 'html5lib')
 	page.close()
-
-	if(page.status_code != 200):
-		print("Problemas ao abrir o site!\n")
-		return False
 
 	#Parent tag
 	general = soup.find(class_ = "entryWrapper")
 
 	#Taking all informations
-	infos = general.find_all('section', attrs = {'class':'gramb'})
+	all_infos = general.find_all('section', attrs = {'class':'gramb'})
 
-	#Word's classes
-	classes = [cl.h3.get_text() for cl in infos]
+	for row in all_infos:
+		info = row.find_all('span', attrs = {'class':["pos", "ind", "iteration", "subsenseIteration"]})
+		divs = [d.get_text() for d in info]
+		for i in divs:
+			print(i)
 
-	#Taking the meanings
-	i = 0
-	for row in general.select('.gramb'):
-		relation_tags = {}
-		meanings = [mg.get_text() for mg in row.select('.ind')]
-		relation_tags[classes[i]] = meanings
-		print('\n', classes[i].upper())
-		for j in relation_tags[classes[i]]:
-			print(j)
-		i = i + 1
-		if i == len(classes):
-			break
+#=====================================================================
 
-linha()
-linha()
-print("\nEncontre o significado de palavras inglesas.\n")
-print("Fazendo scraping de https://en.oxforddictionaries.com :)\nDigite 'sair' para sair.\n")
-exp = 'a'
-while True:
-	linha()
-	linha()
-	exp = input("\nDigite uma palavra em inglês: ")
-	if exp == 'sair':
-		print("Bye :)")
-		break
-	translator(exp)
+mainLoop()
